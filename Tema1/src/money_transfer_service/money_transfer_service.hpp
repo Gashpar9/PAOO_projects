7873fc595de6cb16997e4678f6d9cb079b32b45a
Criste Vlad <vladcriste2@gmail.com>
2023-11-19 22:57:51 +0200

    completed tema 2; changed project architecture to match assignment requests

--- Tema1/src/money_transfer_service/money_transfer_service.hpp
+++ Tema1/src/money_transfer_service/money_transfer_service.hpp
@@ -1,27 +1,108 @@
 #pragma once
 
-class Account {
-    private:
-        char* username;
-        char* password;
-        char* salt;
-        int balance;
+#include <vector>
 
-    public:
-        Account(char* username, char* password);
-        ~Account();
-        Account(const Account& other);
-        Account(Account&& other);
+namespace money_transfer_service {
 
-        bool verifyPassword(char* password);
-        int getBalance();
-        int addBalance(int amount);
-};
+    class Bank_Account {
+        protected:
+            int balance;
+        public:
+            virtual ~Bank_Account() {}
+            
+            virtual Bank_Account* clone() = 0;
 
-class MoneyTransferService {
+            virtual int getBalance();
+            virtual int addBalance(int amount) = 0;
+    };
 
-    public:
-        MoneyTransferService();
+    class Savings_Account : public Bank_Account {
+        private:
+            char* fullname;
+            char* cnp;
+        public:
+            Savings_Account(char* fullname, char* cnp);
+            ~Savings_Account();
+            Savings_Account(const Savings_Account& other);
+            Savings_Account(Savings_Account&& other);
 
-        bool transfer(Account* from, Account* to, int amount, char* password);
-};
+            virtual Savings_Account* clone() override;
+
+            virtual int addBalance(int amount) override;
+    };
+
+    class Business_Account : public Bank_Account {
+        private:
+            char* company_name;
+            char* cif;
+        public:
+            Business_Account(char* company_name, char* cif);
+            ~Business_Account();
+            Business_Account(const Business_Account& other);
+            Business_Account(Business_Account&& other);
+
+            virtual Business_Account* clone() override;
+
+            virtual int addBalance(int amount) override;
+    };
+
+    class User_Account {
+        private:
+            char* username;
+            char* password;
+            char* salt;
+            std::vector<Bank_Account*> accounts;
+        public:
+            User_Account(char* username, char* password);
+            ~User_Account();
+            User_Account(const User_Account& other);
+            User_Account(User_Account&& other);
+
+            User_Account& operator=(const User_Account& other);
+            User_Account& operator=(User_Account&& other);
+
+            bool verifyPassword(char* password);
+            void addAccount(Bank_Account* account);
+            std::vector<Bank_Account*> getAccounts();
+    };
+
+
+
+
+    /*
+    class Account {
+        private:
+            char* username;
+            char* password;
+            char* salt;
+            int balance;
+
+        public:
+            Account(char* username, char* password);
+            ~Account();
+            Account(const Account& other);
+            Account(Account&& other);
+
+            Account& operator=(const Account& other);
+            Account& operator=(Account&& other);
+
+            bool verifyPassword(char* password);
+            int getBalance();
+            int addBalance(int amount);
+    };
+    */
+
+
+
+
+
+
+
+    class MoneyTransferService {
+
+        public:
+            MoneyTransferService();
+
+            bool transfer(User_Account* user, Bank_Account* from, Bank_Account* to, int amount, char* password);
+    };
+}
